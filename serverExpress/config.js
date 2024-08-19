f19d97b2ce2019f91f2b2834a80996781fff1940
Victor Abimbola <abimbolavictor3@gmail.com>
2024-08-19 19:03:21 +0100

    Update config.js

--- serverExpress/config.js
+++ serverExpress/config.js
@@ -16,21 +16,32 @@ module.exports = (app) => {
   app.use(express.static(path.join(__dirname, "/public")));
   app.use(cors());
 
-//   const uri =
-//     "mongodb+srv://otizontezia1778:pSjGtRIb6lKJ1aUy@otscluster.7qlds67.mongodb.net/?retryWrites=true&w=majority";
-//   const client = new MongoClient(uri, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     serverApi: ServerApiVersion.v1,
-//   });
-//   client.connect((err) => {
-//     const collection = client.db("elite_trust").collection("users");
-//     console.log(collection);
-//     // perform actions on the collection object
-//     client.close();
-//   });
+  //   const uri =
+  //     "mongodb+srv://otizontezia1778:pSjGtRIb6lKJ1aUy@otscluster.7qlds67.mongodb.net/?retryWrites=true&w=majority";
+  //   const client = new MongoClient(uri, {
+  //     useNewUrlParser: true,
+  //     useUnifiedTopology: true,
+  //     serverApi: ServerApiVersion.v1,
+  //   });
+  //   client.connect((err) => {
+  //     const collection = client.db("elite_trust").collection("users");
+  //     console.log(collection);
+  //     // perform actions on the collection object
+  //     client.close();
+  //   });
 
-   mongoose.connect(
+  // mongoose.connect("mongodb+srv://otizontezia1778:pSjGtRIb6lKJ1aUy@otscluster.7qlds67.mongodb.net/elite_trust?retryWrites=true&w=majority", {
+  //   useUnifiedTopology: true,
+  //   useNewUrlParser: true
+  // }, (err) => {
+  //   if(err){
+  //     throw err;
+  //   }
+  //   console.log('Database is started')
+
+  // });
+
+  mongoose.connect(
     "mongodb+srv://alarconkelly38:4marketDaysP2023@cluster0.u4dwhay.mongodb.net/?retryWrites=true&w=majority",
     {
       useUnifiedTopology: true,
